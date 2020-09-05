6077f149fa3fcfe28a248e6b331ba287aa65062c
sapinspys <garri009@fiu.edu>
2020-09-04 21:38:53 -0400

    Created the room class

--- Intro-Python-II/src/room.py
+++ Intro-Python-II/src/room.py
@@ -1,2 +1,11 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.n_to = False
+        self.s_to = False
+        self.e_to = False
+        self.w_to = False
