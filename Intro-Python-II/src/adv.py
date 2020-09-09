ca3e28f1a896e3e77f370925bc0869d4fa12066c
sapinspys <garri009@fiu.edu>
2020-09-08 22:10:31 -0400

    Added quit on q option

--- Intro-Python-II/src/adv.py
+++ Intro-Python-II/src/adv.py
@@ -1,11 +1,11 @@
 import sys
+import os
 import time
 import textwrap
 from room import Room
 from player import Player
 
 # Declare all the rooms
-
 room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
@@ -27,7 +27,6 @@ earlier adventurers. The only exit is to the south."""),
 
 
 # Link rooms together
-
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
@@ -44,70 +43,78 @@ def steady_print(text):
         time.sleep(.05)
     return ""
 
-
-def display_location(loc):
-    print("")
-    print(steady_print(f"Current location: {loc.name}"))
-    print(loc.description)
-    print("")
-
-
-def outside_choice():
-    options = ["y", "n"]
-    choice = ""
-    while choice not in options:
-        choice = input("Do you dare enter the cave? [y, n]: ")
-        print("")
-        if choice == "y":
-            global p1
-            p1.current_room = room["foyer"]
-            return steady_print("You take a deep breath and enter...\n")
-        elif choice == "n":
-            print(steady_print("Game Over."))
-            quit()
-        else:
-            print(steady_print("I didn't understand that."))
-
-
-def in_cave_choice(location):
-    rooms = {
-        "n": location.n_to,
-        "s": location.s_to,
-        "e": location.e_to,
-        "w": location.w_to
-    }
-
-    while True:
-        choice = input("Which way do you go? [n, s, e, w]: ")
-        next_room = rooms[choice]
-        if next_room:
-            global p1
-            p1.current_room = next_room
-            return ""
-        else:
-            print(steady_print(
-                f"\nYou can't go in this direction so you return to the {location.name}...\n"))
-
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
 
 #
 # Main
 #
+def start_game():
+    def display_location(loc):
+        print("")
+        print(steady_print(f"Current location: {loc.name}"))
+        print(loc.description)
+        print("")
 
-# Start
-print(steady_print("Welcome treasure hunter!"))
-player_name = input("Enter your name: ")
+    def outside_choice():
+        options = ["y", "n"]
+        choice = ""
+        while choice not in options:
+            choice = input("Do you dare enter the cave? [y, n]: ")
+            print("")
+            if choice == "y":
+                nonlocal p1
+                p1.current_room = room["foyer"]
+                return steady_print("You take a deep breath and enter...\n")
+            elif choice == "n":
+                print(steady_print("Game Over."))
+                quit()
+            elif choice == "q":
+                print(steady_print("Game Over."))
+                quit()
+            else:
+                print(steady_print("I didn't understand that."))
 
-p1 = Player(player_name, room["outside"])
+    def in_cave_choice(location):
+        rooms = {
+            "n": location.n_to,
+            "s": location.s_to,
+            "e": location.e_to,
+            "w": location.w_to
+        }
 
-# Game
-while True:
-    location = p1.current_room
-    if location.name == room["outside"].name:
-        display_location(location)
-        outside_choice()
-    else:
-        display_location(location)
-        in_cave_choice(location)
+        while True:
+            choice = input("Which way do you go? [n, s, e, w]: ")
+            if choice == "q":
+                print(steady_print("\nGame Over."))
+                quit()
+            else:
+                next_room = rooms[choice]
+
+            if next_room:
+                nonlocal p1
+                p1.current_room = next_room
+                return ""
+            else:
+                print(steady_print(
+                    f"\nYou can't go in this direction so you return to the {location.name}...\n"))
+
+    cls()
+    print(steady_print("Welcome treasure hunter!"))
+    player_name = input("Enter your name: ")
+    
+    print(steady_print("\nEnter 'q' during any input to exit game."))
+
+    p1 = Player(player_name, room["outside"])
+
+    while True:
+        location = p1.current_room
+        if location.name == room["outside"].name:
+            display_location(location)
+            outside_choice()
+        else:
+            display_location(location)
+            in_cave_choice(location)
 
 
 # Make a new player object that is currently in the 'outside' room.
@@ -122,3 +129,6 @@ while True:
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+if __name__ == '__main__':
+    start_game()
