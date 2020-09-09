71a9155fb1bee93e05aa5ee210981830d24366b3
sapinspys <garri009@fiu.edu>
2020-09-09 14:11:58 -0400

    Added item on_take and on_drop

--- Intro-Python-II/src/item.py
+++ Intro-Python-II/src/item.py
@@ -2,3 +2,9 @@ class Item:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+
+    def on_take(self):
+        return f"You have picked up {self.name}."
+
+    def on_drop(self):
+        return f"You have dropped {self.name}."
