0906051e4ebe7d2334c23c92a474d1fbb3b0e236
sapinspys <garri009@fiu.edu>
2020-09-10 00:08:51 -0400

    Added pick up item functionality

--- Intro-Python-II/src/adv.py
+++ Intro-Python-II/src/adv.py
@@ -57,26 +57,30 @@ def cls():
 
 
 def start_game():
-    def display_location(loc):
-        print(steady_print(f"Current location: {loc.name}"))
-        print(loc.description)
+    def display_location(player):
+        current_room = player.current_room
+        print(steady_print(f"Current location: {current_room.name}"))
+        print(current_room.description)
         print("")
-        print(steady_print(f"Items available here: {loc.items}"))
+        print(steady_print(f"Items available here: {current_room.items}"))
+        print(steady_print(f"Player inventory: {player.inventory}"))
         print("")
 
-    def choose_action(room):
+    def choose_action(player):
+        current_room = player.current_room
         available_rooms = {
-            "n": room.n_to,
-            "s": room.s_to,
-            "e": room.e_to,
-            "w": room.w_to
+            "n": current_room.n_to,
+            "s": current_room.s_to,
+            "e": current_room.e_to,
+            "w": current_room.w_to
         }
 
         while True:
             choice = input(
                 "What would you like to do?\n[n, s, e, w or <take, get, drop> item]: ")
             print("")
-            word_ct = len(choice.split(" "))
+            word_split = choice.split(" ")
+            word_ct = len(word_split)
             if choice == "q":
                 print(steady_print("Game Over!"))
                 print("")
@@ -84,16 +88,27 @@ def start_game():
             elif word_ct == 1:
                 next_room = available_rooms[choice]
                 if next_room:
-                    nonlocal p1
-                    p1.current_room = next_room
+                    player.current_room = next_room
                     return ""
                 else:
                     print(steady_print(
                         f"You can't go in this direction so you return to the {room.name}..."))
                     print("")
             elif word_ct == 2:
-                print(f"{choice}")
-                print("")
+                verb = word_split[0]
+                item = word_split[1]
+                if verb == "take" or verb == "get":
+                    items = current_room.items
+                    if item in items:
+                        current_room.remove_item(item)
+                        player.add_item(item)
+                    else:
+                        print(steady_print(f"There's no {item} in this room.."))
+                elif verb == "drop":
+                    pass
+                else:
+                    print(steady_print(f"Unable to {choice}!"))
+                    print("")
             else:
                 print(steady_print("Unknown command..."))
                 print("")
@@ -108,9 +123,8 @@ def start_game():
     print("")
 
     while True:
-        current_room = p1.current_room
-        display_location(current_room)
-        choose_action(current_room)
+        display_location(p1)
+        choose_action(p1)
 
 
 if __name__ == '__main__':
