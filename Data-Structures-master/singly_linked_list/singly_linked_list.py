c82a1664307a002e92da3df16e726ddf68123cc0
sapinspys <garri009@fiu.edu>
2020-09-15 22:58:33 -0400

    Updated linked list methods

--- Data-Structures-master/singly_linked_list/singly_linked_list.py
+++ Data-Structures-master/singly_linked_list/singly_linked_list.py
@@ -26,15 +26,32 @@ class LinkedList:
         else:
             # Set next_node of new_node to current head
             new_node.set_next_node(self.head)
-
             # Update current head to new_node
             self.head = new_node
 
     def add_to_tail(self, value):
-        # TODO
-
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        elif self.head == self.tail:
+            self.head.set_next_node(new_node)
+            self.tail = new_node
+        else:
+            self.tail.set_next_node(new_node)
+            self.tail = new_node
     def remove_head(self):
-        # TODO
+        # Cases?
+        if self.head is None:
+            return None
+        else:
+            ret_value = self.head.get_value()
+            if self.head == self.tail:
+                self.head = None
+                self.tail = None
+            else:
+                self.head = self.head.get_next_node()
+            return ret_value
 
     def remove_tail(self):
         # TODO
