5ada9362b16ba1e0914e7e1d2750da196b2bff74
sapinspys <garri009@fiu.edu>
2020-09-15 22:27:45 -0400

    Updated linked list classes

--- Data-Structures-master/singly_linked_list/singly_linked_list.py
+++ Data-Structures-master/singly_linked_list/singly_linked_list.py
@@ -1,11 +1,11 @@
 class Node:
-    def __init__(self, value, next_node):
+    def __init__(self, value=None, next_node=None):
         self.value = value
         self.next_node = next_node
 
     def get_value(self):
         return self.value
-    
+
     def get_next_node(self):
         return self.next_node
 
@@ -14,5 +14,31 @@ class Node:
 
 
 class LinkedList:
-    def __init__(self, list):
-        pass
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_to_head(self, value):
+        # Create new node
+        new_node = Node(value)
+
+        # Set next_node of new_node to current head
+        new_node.set_next_node(self.head)
+
+        # Update current head to new_node
+        self.head = new_node
+
+    def add_to_tail(self, value):
+        # TODO
+
+    def remove_head(self):
+        # TODO
+
+    def remove_tail(self):
+        # TODO
+
+    def contains(self, value):
+        # TODO time permitting
+
+    def get_max(self):
+        # TODO time permitting
