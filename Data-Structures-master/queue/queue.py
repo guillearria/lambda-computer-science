93d4544a4783870e52b0e0faf30c3b2f98498eca
sapinspys <garri009@fiu.edu>
2020-09-16 21:42:32 -0400

    Queue stretch completed

--- Data-Structures-master/queue/queue.py
+++ Data-Structures-master/queue/queue.py
@@ -38,33 +38,54 @@ Stretch: What if you could only use instances of your Stack class to implement t
 # 2. Using linked list as underlying structure
 
 
-from singly_linked_list import LinkedList
+# from singly_linked_list import LinkedList
 
 
-class Queue:
-    def __init__(self):
-        self.size = 0
-        self.storage = LinkedList()
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = LinkedList()
 
-    def __len__(self):
-        return self.size
+#     def __len__(self):
+#         return self.size
 
-    def enqueue(self, value):
-        self.size += 1
-        self.storage.add_to_tail(value)
+#     def enqueue(self, value):
+#         self.size += 1
+#         self.storage.add_to_tail(value)
 
-    def dequeue(self):
-        if self.size:
-            self.size -= 1
-            removed_head_val = self.storage.remove_head()
-            return removed_head_val
-        else:
-            return None
+#     def dequeue(self):
+#         if self.size:
+#             self.size -= 1
+#             removed_head_val = self.storage.remove_head()
+#             return removed_head_val
+#         else:
+#             return None
 
 
 # 3. What is the difference between using an array vs. a linked list when
 # implementing a Queue?
 
-""" The difference is not having to use self.size with an array since they come with
-built-in len method. Append and pop are very similar to the add and remove methods
+""" The difference is not having to use self.size with an array since it comes with
+built-in len method. Append and pop are also very similar to the add and remove methods
 of a LinkedList."""
+
+# 4. Stretch: What if you could only use instances of your Stack class to implement the Queue?
+# What would that look like? How many Stacks would you need? Try it!
+
+
+from stack import Stack
+
+
+class Queue:
+    def __init__(self):
+        self.storage = Stack()
+
+    def __len__(self):
+        return len(self.storage)
+
+    def enqueue(self, value):
+        self.storage.size += 1
+        self.storage.storage.add_to_tail(value)
+
+    def dequeue(self):
+        return self.storage.pop()
