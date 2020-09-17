61b036bc924f8708bbc10a51a2d6e433c387d018
sapinspys <garri009@fiu.edu>
2020-09-16 21:04:10 -0400

    Implemented get length for LinkedList based Queue

--- Data-Structures-master/queue/queue.py
+++ Data-Structures-master/queue/queue.py
@@ -13,15 +13,47 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
-# Using array as underlying structure  
+# 1. Using array as underlying structure
 # Remove from head, add to tail
+
+
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def enqueue(self, value):
+#         self.storage.append(value)
+
+#     def dequeue(self):
+#         if len(self.storage):
+#             popped_element = self.storage.pop(0)
+#             return popped_element
+#         else:
+#             return None
+
+from singly_linked_list import LinkedList
+# 2. Using linked list as underlying structure
+
+
 class Queue:
     def __init__(self):
         self.size = 0
-        self.storage = []
-    
+        self.storage = LinkedList()
+
     def __len__(self):
-        return len(self.storage)
+        count = 0
+        if self.storage.head is None:
+            return count
+        else:
+            cur_node = self.storage.head
+            while cur_node is not None:
+                cur_node = cur_node.get_next_node()
+                count += 1
+            return count
 
     def enqueue(self, value):
         self.storage.append(value)
