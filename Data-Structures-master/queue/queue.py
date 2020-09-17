a45ea93ff067d9cc24c7df2ed681754a0376cb78
sapinspys <garri009@fiu.edu>
2020-09-16 20:53:44 -0400

    Completed Queue mvp using array

--- Data-Structures-master/queue/queue.py
+++ Data-Structures-master/queue/queue.py
@@ -13,16 +13,22 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+# Using array as underlying structure  
+# Remove from head, add to tail
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
     
     def __len__(self):
-        pass
+        return len(self.storage)
 
     def enqueue(self, value):
-        pass
+        self.storage.append(value)
 
     def dequeue(self):
-        pass
+        if len(self.storage):
+            popped_element = self.storage.pop(0)
+            return popped_element
+        else:
+            return None
