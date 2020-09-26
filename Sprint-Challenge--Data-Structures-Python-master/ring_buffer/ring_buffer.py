3c2ba3921e90915d2e52a1b21808e88a3acdf1a7
sapinspys <garri009@fiu.edu>
2020-09-25 21:44:16 -0400

    Built first attempt at RingBuffer append

--- Sprint-Challenge--Data-Structures-Python-master/ring_buffer/ring_buffer.py
+++ Sprint-Challenge--Data-Structures-Python-master/ring_buffer/ring_buffer.py
@@ -1,7 +1,8 @@
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.storage = []
+        self.storage = {}
+        self.order = 1
         
     def append(self, item):
         # adds element to buffer
@@ -10,8 +11,14 @@ class RingBuffer:
         # else if full
             # remove oldest value
             # replace with new value
-        pass
-
+        if len(x) < self.capacity:
+            self.storage[item] = self.order
+        else:
+            oldest_item = min(self.storage, key=self.storage.get)
+            self.storage[item] = self.storage[oldest_item].pop
+            self.storage[item] = self.order
+        self.order += 1
+        
     def get(self):
         # return the list
         pass
