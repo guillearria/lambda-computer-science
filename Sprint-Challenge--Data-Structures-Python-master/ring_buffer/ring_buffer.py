993e3b4fb0d122dfe1293c68689c1cf11dac336a
sapinspys <garri009@fiu.edu>
2020-09-25 21:31:02 -0400

    Added non-code logic for RingBuffer append and get

--- Sprint-Challenge--Data-Structures-Python-master/ring_buffer/ring_buffer.py
+++ Sprint-Challenge--Data-Structures-Python-master/ring_buffer/ring_buffer.py
@@ -4,7 +4,14 @@ class RingBuffer:
         self.storage = []
         
     def append(self, item):
+        # adds element to buffer
+        # if capacity is not full
+            # append to end of list
+        # else if full
+            # remove oldest value
+            # replace with new value
         pass
 
     def get(self):
+        # return the list
         pass
