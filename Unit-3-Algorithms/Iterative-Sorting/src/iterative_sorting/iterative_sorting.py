05c6fe4285bf927f71ae0512cdc37e14086ca706
sapinspys <garri009@fiu.edu>
2020-09-30 18:36:37 -0400

    Minor selection sort readability fix

--- Unit-3-Algorithms/Iterative-Sorting/src/iterative_sorting/iterative_sorting.py
+++ Unit-3-Algorithms/Iterative-Sorting/src/iterative_sorting/iterative_sorting.py
@@ -7,9 +7,9 @@ def selection_sort(arr):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
-        for i in range(cur_index, len(arr)):
-            if arr[i] < arr[smallest_index]:
-                smallest_index = i
+        for j in range(cur_index, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
         # TO-DO: swap
         arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
