77310e1bb10dda04d2b911212ee09c319d128796
sapinspys <garri009@fiu.edu>
2020-09-30 18:42:49 -0400

    Implented first attempt at linear search

--- Unit-3-Algorithms/Iterative-Sorting/src/searching/searching.py
+++ Unit-3-Algorithms/Iterative-Sorting/src/searching/searching.py
@@ -1,6 +1,11 @@
 def linear_search(arr, target):
-    # Your code here
-
+    # Given a set of data, traverse the dataset one item 
+    # at a time until either you find the item you are 
+    # looking for OR check every item in the set and verify 
+    # the item you are looking for is not there.
+    for i, item in enumerate(arr):
+        if item == target:
+            return i
 
     return -1   # not found
 
