1b1ecaf1e43286ea04d3421fcedd2af18994c1ae
sapinspys <garri009@fiu.edu>
2020-10-02 19:44:41 -0400

    Minor code challenge update

--- Code-Challenges/uniqueCharacters.py
+++ Code-Challenges/uniqueCharacters.py
@@ -1,27 +1,18 @@
 # Write a function that counts unique characters in a given string Note: spaces and special characters (!"/@;&) may count as a unique character
 
 # Example:
-
 # Input: "HeLlo WorLd"
-
 # Output: 8
 
-
 # Good questions to ask when in an interview:
-
 # Are uppercase and lowercase of the same letter considered a unique character? (In this case yes)
-
 # Are alphanumerical numbers only being counted? Or special characters as well?
 
 def uniqueCharacters(s):
     return len(set([char for char in s]))
 
 # Example tests:
-
-uniqueCharacters("hello") # 4
-
-uniqueCharacters("HeLlo WorLd") # 8
-
-uniqueCharacters("gOoD MORninG") # 10
-
-uniqueCharacters("Do Special Characters Count???") # 16
+print(uniqueCharacters("hello"))  # 4
+print(uniqueCharacters("HeLlo WorLd"))  # 9
+print(uniqueCharacters("gOoD MORninG"))  # 10
+print(uniqueCharacters("Do Special Characters Count???"))  # 18
