9b3fba2adcc4b3de762004087ba16967bc41a7c9
sapinspys <garri009@fiu.edu>
2020-10-01 21:44:58 -0400

    Added recursive sorting project

---
+++ Unit-3-Algorithms/Recursive-Sorting/src/sorting/sorting.py
@@ -0,0 +1,28 @@
+# TO-DO: complete the helper function below to merge 2 sorted arrays
+def merge(arrA, arrB):
+    elements = len(arrA) + len(arrB)
+    merged_arr = [0] * elements
+
+    # Your code here
+
+
+    return merged_arr
+
+# TO-DO: implement the Merge Sort function below recursively
+def merge_sort(arr):
+    # Your code here
+
+
+    return arr
+
+# STRETCH: implement the recursive logic for merge sort in a way that doesn't 
+# utilize any extra memory
+# In other words, your implementation should not allocate any additional lists 
+# or data structures; it can only re-use the memory it was given as input
+def merge_in_place(arr, start, mid, end):
+    # Your code here
+
+
+def merge_sort_in_place(arr, l, r):
+    # Your code here
+
