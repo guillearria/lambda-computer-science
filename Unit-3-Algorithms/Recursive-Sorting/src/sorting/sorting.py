d6848d0ad3ec5003a0f35c4bec4bf63dba190fbd
sapinspys <garri009@fiu.edu>
2020-10-01 22:22:42 -0400

    Implemented merge  helper

--- Unit-3-Algorithms/Recursive-Sorting/src/sorting/sorting.py
+++ Unit-3-Algorithms/Recursive-Sorting/src/sorting/sorting.py
@@ -1,11 +1,8 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
-
-    # Your code here
-
-
+    # elements = len(arrA) + len(arrB)
+    # merged_arr = [0] * elements
+    merged_arr = arrA + arrB
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
@@ -19,10 +16,10 @@ def merge_sort(arr):
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists 
 # or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
-    # Your code here
+# def merge_in_place(arr, start, mid, end):
+#     # Your code here
 
 
-def merge_sort_in_place(arr, l, r):
-    # Your code here
+# def merge_sort_in_place(arr, l, r):
+#     # Your code here
 
