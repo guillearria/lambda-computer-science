693bbea372e4b8e5a98bdb693a78baf7c9147599
sapinspys <garri009@fiu.edu>
2020-10-02 20:22:57 -0400

    First attempt at recursive merge sort

--- Unit-3-Algorithms/Recursive-Sorting/src/sorting/sorting.py
+++ Unit-3-Algorithms/Recursive-Sorting/src/sorting/sorting.py
@@ -1,7 +1,29 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
+    # initialize empty array
+    merged_arr = []
+
+    # keep track of where we are in each array
+    indexA = 0
+    indexB = 0
+
+    # go through both copies until we run out of elements
+    while indexA < len(arrA)-1 and indexB < len(arrB)-1:
+        # if arrA has smaller element, append to sorted, update pointer
+        if arrA[indexA] <= arrB[indexB]:
+            merged_arr.append(arrA[indexA])
+            indexA += 1
+        else:
+            merged_arr.append(arrB[indexB])
+            indexB += 1
+
+    # if we run out of one list, continue with other list
+    while indexA < len(arrA)-1:
+        merged_arr.append(arrA[indexA])
+        indexA += 1
+    while indexB < len(arrB)-1:
+        merged_arr.append(arrB[indexB])
+        indexB += 1
 
     return merged_arr
 
@@ -9,14 +31,18 @@ def merge(arrA, arrB):
 def merge_sort(arr):
     if len(arr) <= 1:
         return arr
-    
-    
 
-    return arr
+    start = 0
+    end = len(arr)-1
+    mid = (start+end)//2
+    merge_sort(arr[0:mid])
+    merge_sort(arr[mid:])
+    merge(arr[0:mid], arr[mid:])
 
-# STRETCH: implement the recursive logic for merge sort in a way that doesn't 
+
+# STRETCH: implement the recursive logic for merge sort in a way that doesn't
 # utilize any extra memory
-# In other words, your implementation should not allocate any additional lists 
+# In other words, your implementation should not allocate any additional lists
 # or data structures; it can only re-use the memory it was given as input
 # def merge_in_place(arr, start, mid, end):
 #     # Your code here
@@ -25,3 +51,16 @@ def merge_sort(arr):
 # def merge_sort_in_place(arr, l, r):
 #     # Your code here
 
+
+# Merge sort
+# ----------
+# Phase 1: calling down the recursion:
+#           [5 9 3 7 2 8 1 6]
+#      [5 9 3 7]         [2 8 1 6]
+#     [5 9] [3 7]       [2 8] [1 6]
+#   [5] [9] [3] [7]   [2] [8] [1] [6]
+# Phase 2: merge! returning from recursion:
+#   [5] [9] [3] [7]   [2] [8] [1] [6]
+#     [5 9] [3 7]       [2 8] [1 6]
+#      [3 5 7 9]         [1 2 6 8]
+#          [1 2 3 5 6 7 8 9]
