27f86ba5b389e76832a990a3e10c9cb5701c2be3
sapinspys <garri009@fiu.edu>
2020-10-07 00:37:56 -0400

    Attempt 1 at single number, failed

--- Unit-3-Algorithms/Advanced-Problems/single_number/single_number.py
+++ Unit-3-Algorithms/Advanced-Problems/single_number/single_number.py
@@ -3,10 +3,16 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
-
-    pass
-
+    # Attempt 1
+    # loop through arr, get i
+        # pop item at that i
+        # check if pop value is in arr
+        # return pop value if yes
+    # dup_arr = arr.copy()
+    # for num in arr:
+    #     dup_arr.remove(num)
+    #     if num not in dup_arr:
+    #         return num
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
