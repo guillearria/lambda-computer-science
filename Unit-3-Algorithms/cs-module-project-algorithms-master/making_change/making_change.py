a76b94b6b1eb798f704fc459fe83ef0e2412e04a
sapinspys <garri009@fiu.edu>
2020-10-06 20:40:48 -0400

    Added unit3, modules 3 and 4 projects

---
+++ Unit-3-Algorithms/cs-module-project-algorithms-master/making_change/making_change.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import sys
+
+def making_change(amount, denominations):
+  # Your code here
+
+  pass
+
+
+if __name__ == "__main__":
+  # Test our your implementation from the command line
+  # with `python making_change.py [amount]` with different amounts
+  if len(sys.argv) > 1:
+    denominations = [1, 5, 10, 25, 50]
+    amount = int(sys.argv[1])
+    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
+  else:
+    print("Usage: making_change.py [amount]")
---
+++ Unit-3-Algorithms/cs-module-project-algorithms-master/rock_paper_scissors/rps.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import sys
+
+def rock_paper_scissors(n):
+  # Your code here
+
+  pass
+
+
+if __name__ == "__main__":
+  if len(sys.argv) > 1:
+    num_plays = int(sys.argv[1])
+    print(rock_paper_scissors(num_plays))
+  else:
+    print('Usage: rps.py [num_plays]')
