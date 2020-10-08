1693008ac79df881996c07b4dfdf7a93881fc8f0
sapinspys <garri009@fiu.edu>
2020-10-07 23:36:09 -0400

    Optimized single_number function

--- Unit-3-Algorithms/Advanced-Problems/single_number/single_number.py
+++ Unit-3-Algorithms/Advanced-Problems/single_number/single_number.py
@@ -2,38 +2,52 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+import time
+import random
+
 def single_number(arr):
-    # Attempt 1
-    # loop through arr, get i
-        # pop item at that i
-        # check if pop value is in arr
-        # return pop value if yes
-    # dup_arr = arr.copy()
-    # for num in arr:
-    #     dup_arr.remove(num)
-    #     if num not in dup_arr:
-    #         return num
+    start = time.perf_counter()
+    # First pass solution:
+        # Time complexity: O(n^2)
+        # Space complexity: 
 
-    # Attempt 2, inefficient
-    # init count dictionary
-    # loop through arr, nums
-        # if num  in keys, add to count
-        # else, create new key with val 1
-    # look for min value of dictionary, return key
-
-    # Attempt 3
     # init set of arr
     # loop through set
         # remove num from arr
         # if num not in arr, return num
-    dups_removed = set(arr)
-    for num in dups_removed:
-        arr.remove(num)
-        if num not in arr:
-            return num
+
+    # dups_removed = set(arr)
+    # for num in dups_removed:
+    #     arr.remove(num)
+    #     if num not in arr:
+    #         end = time.perf_counter()
+    #         print("Runtime:", end-start, "seconds")
+    #         return num
+
+    # Optimized solution:
+        # how can we keep track of duplicates?
+        # use hash table
+            # {
+            #   signature: [duplicates]
+            # }
+
+    result = {}
+
+    for num in arr:
+        signature = num
+        if signature not in result:
+            result[signature] = 1
+        else:
+            del result[signature]
+
+    end = time.perf_counter()
+    print("Runtime:", end-start, "seconds")
+    # next, iter faster than list()
+    return next(iter(result))
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
+    random.shuffle(arr)
 
     print(f"The odd-number-out is {single_number(arr)}")
