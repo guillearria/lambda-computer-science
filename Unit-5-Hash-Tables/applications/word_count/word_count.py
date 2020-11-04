f3ebd3589dd8f3ae95c7a7c413fddc013ecf227a
sapinspys <garri009@fiu.edu>
2020-11-04 01:04:31 -0500

    word count challenge complete

--- Unit-5-Hash-Tables/applications/word_count/word_count.py
+++ Unit-5-Hash-Tables/applications/word_count/word_count.py
@@ -1,5 +1,33 @@
 def word_count(s):
-    # Your code here
+    # initate dictionary
+    words = {}
+    ignored = ['"', ':', ';', ',', '.', '-', '+', '=', '/', '\\', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
+    special = ['\r', '\n', '\t']
+
+    # remove special characters
+    s=s.replace(special[0], " ")
+    s=s.replace(special[1], " ")
+    s=s.replace(special[2], " ")
+    
+    # remove punctuation
+    s = "".join([char for char in s if char not in ignored])
+
+    # check if empty
+    if s:
+        # split string into words
+        word_list = s.split(" ")
+        # lowercase all words & remove spaces
+        word_list = [word.lower() for word in word_list if word]
+
+        # iterate and add to dictionary
+        for i in word_list:
+            if i in words:
+                words[i] += 1
+            else: 
+                words[i] = 1
+
+        return words
+    else: return words 
 
 
 
