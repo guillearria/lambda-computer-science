60380994126be602b97d5b49a9b4f552e9aa4b50
sapinspys <garri009@fiu.edu>
2020-11-04 00:15:37 -0500

    expensive sequence challenge completed

--- Unit-5-Hash-Tables/applications/expensive_seq/expensive_seq.py
+++ Unit-5-Hash-Tables/applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,15 @@
-# Your code here
-
+hashtable = {}
 
 def expensive_seq(x, y, z):
-    # Your code here
+    if x <= 0: 
+        return y + z
+    if x >  0: 
+        idx = (x,y,z)
+        
+        if idx not in hashtable:
+            hashtable[idx] = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
 
+        return hashtable[idx]
 
 
 if __name__ == "__main__":
