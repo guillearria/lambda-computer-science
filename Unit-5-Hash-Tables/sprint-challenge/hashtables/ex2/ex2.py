708f59251bd612626e84473d439fa5501a241929
sapinspys <garri009@fiu.edu>
2020-11-06 22:58:04 -0500

    Completed ex2 challenge

--- Unit-5-Hash-Tables/sprint-challenge/hashtables/ex2/ex2.py
+++ Unit-5-Hash-Tables/sprint-challenge/hashtables/ex2/ex2.py
@@ -7,8 +7,29 @@ class Ticket:
 
 def reconstruct_trip(tickets, length):
     """
-    YOUR CODE HERE
-    """
-    # Your code here
+    Reconstruct ticket order given array of ticket classes. Source is start,
+        dest is end.
 
-    return route
+        First flight = source of None
+        Final flight = destination of None
+
+    Returns array of strings with entire route by airports.
+    """
+    # convert ticket list into a dictionary
+    # set cur_flight to None
+    # while x, find dest of cur_flight
+        # add dest to flight_route  
+        # add dest as cur_flight
+    # return flight_route
+
+    ticket_store = { ticket.source:ticket.destination for ticket in tickets }
+    flight_route = []
+    cur_flight = "NONE"
+    cur_dest = ""
+
+    while cur_dest is not "NONE":
+        cur_dest = ticket_store[cur_flight]
+        flight_route.append(cur_dest)
+        cur_flight = cur_dest
+
+    return flight_route
