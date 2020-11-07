cf3e6280bd8d43e015d5856fc49da8c8e71967d2
sapinspys <garri009@fiu.edu>
2020-11-06 22:36:26 -0500

    Completed ex1 challenge

--- Unit-5-Hash-Tables/sprint-challenge/hashtables/ex1/ex1.py
+++ Unit-5-Hash-Tables/sprint-challenge/hashtables/ex1/ex1.py
@@ -1,7 +1,24 @@
 def get_indices_of_item_weights(weights, length, limit):
     """
-    YOUR CODE HERE
-    """
-    # Your code here
+    Givn package with weight 'limit' and list of item 'weights,'
+        return two item index tuple whose sum of weights equals the limit.
 
-    return None
+    Higher value item goes in index 0, and smaller in index 1. If no pair exists,
+        return None.
+    """
+    results = []
+
+    for idx, wt in enumerate(weights):
+        matching_wt = limit - wt
+        if matching_wt in weights:
+            results.extend([weights.index(matching_wt, idx+1), idx])
+            break
+        
+    if results:
+        results = sorted(results, reverse=True)
+        return tuple(results)
+    else:
+        return None
+
+
+
