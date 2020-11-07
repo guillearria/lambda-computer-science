111d527892f979ccc94c5f91b0657f64e0d3f206
sapinspys <garri009@fiu.edu>
2020-11-06 23:38:50 -0500

    Completed ex5 challenge

--- Unit-5-Hash-Tables/sprint-challenge/hashtables/ex5/ex5.py
+++ Unit-5-Hash-Tables/sprint-challenge/hashtables/ex5/ex5.py
@@ -4,11 +4,23 @@
 
 def finder(files, queries):
     """
-    YOUR CODE HERE
+    Accepts a list of file paths and list of queries, or file names. Return paths
+        for those files that exist in the system.
     """
-    # Your code here
+    path_table = {}
+    for path in files:
+        file_name = path.split("/")[-1]
+        if file_name in path_table:
+            path_table[file_name].append(path)
+        else:
+            path_table[file_name] = [path]
+    
+    results = []
+    for query in queries:
+        if query in path_table:
+            results.extend(path_table[query])
 
-    return result
+    return results
 
 
 if __name__ == "__main__":
