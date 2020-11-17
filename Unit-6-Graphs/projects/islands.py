7e52a283ee10310223aebdec8fb39db1cf075a1c
sapinspys <garri009@fiu.edu>
2020-11-16 23:14:05 -0500

    Completed islands challenge

--- Unit-6-Graphs/projects/islands.py
+++ Unit-6-Graphs/projects/islands.py
@@ -27,9 +27,67 @@ island_counter(islands) # returns 4
             - Keep track of visited 1s
 """
 
-def island_counter(islands):
-    pass
+# GET NEIGHBORS FUNC
 
+
+def getNeighbors(row, col, matrix):
+    neighbors = []
+
+    if col >= 1:
+        west = matrix[row][col - 1]
+        if west == 1:
+            neighbors.append((row, col - 1))
+
+    if col <= len(matrix) - 2:
+        east = matrix[row][col + 1]
+        if east == 1:
+            neighbors.append((row, col + 1))
+
+    if row <= len(matrix) - 2:
+        south = matrix[row + 1][col]
+        if south == 1:
+            neighbors.append((row + 1, col))
+
+    if row >= 1:
+        north = matrix[row - 1][col]
+        if north == 1:
+            neighbors.append((row - 1, col))
+
+    return neighbors
+
+# TRAVERSAL FUNC
+
+
+def dft_recursive(row, col, visited, matrix):
+    if (row, col) not in visited:
+        visited.add((row, col))
+
+        neighbors = getNeighbors(row, col, matrix)
+
+        for neighbor in neighbors:
+            dft_recursive(neighbor[0], neighbor[1], visited, matrix)
+
+
+def island_counter(matrix):
+    # iterate over matrix:
+        # if 0, continue
+        # if 1, traverse neighbors, add to visited, continue iteration
+
+    visited = set()
+    conn_component_count = 0
+
+    for row in range(len(matrix)):
+        for col in range(len(matrix)):
+            node = matrix[row][col]
+
+            if node == 0:
+                continue
+            elif node == 1:
+                if (row, col) not in visited:
+                    conn_component_count += 1
+                    dft_recursive(row, col, visited, matrix)
+
+    return conn_component_count
 
 
 islands = [[0, 1, 0, 1, 0],
