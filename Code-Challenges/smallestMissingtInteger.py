76d7714c4888286870e1be1ee1d4e84cac9a4ac1
sapinspys <garri009@fiu.edu>
2021-02-03 15:41:00 -0500

    Added a time efficient code challenge solution

--- Code-Challenges/smallestMissingtInteger.py
+++ Code-Challenges/smallestMissingtInteger.py
@@ -18,6 +18,40 @@ Write an efficient algorithm for the following assumptions:
         each element of array A is an integer within the range [−1,000,000..1,000,000]."""
 
 # V1, O(N**2), HIGH CORRECTNESS, LOW PERFORMANCE
+# def solution(A):
+#     """
+#     Given array A, returns smallest positive integer, > 0, that does not occur in array A.
+    
+#     Cases:
+#         len(A) = 0, always returns 1
+#         len(A) > 0
+    
+#     A - array of N integers
+#     """
+#     # if len A is 0, return 1
+#     # else:
+#         # get min and max of array
+#         # if both are < 0, return 1
+#         # else:
+#             # iterate through range(1,max+1):
+#                 # if num not in A:
+#                     # return num
+#             # return max + 1 if list is good
+            
+#     if len(A) == 0:
+#         return 1
+#     else:
+#         if min(A) < 0 and max(A) < 0:
+#             return 1
+#         else:
+#             for n in range(1, max(A)+1):
+#                 if n not in A:
+#                     return n
+#             return max(A)+1
+
+
+# V2, time efficient
+# Testing for the presence of a number in a set is FAST in Python
 def solution(A):
     """
     Given array A, returns smallest positive integer, > 0, that does not occur in array A.
@@ -28,23 +62,12 @@ def solution(A):
     
     A - array of N integers
     """
-    # if len A is 0, return 1
-    # else:
-        # get min and max of array
-        # if both are < 0, return 1
-        # else:
-            # iterate through range(1,max+1):
-                # if num not in A:
-                    # return num
-            # return max + 1 if list is good
-            
-    if len(A) == 0:
-        return 1
-    else:
-        if min(A) < 0 and max(A) < 0:
-            return 1
-        else:
-            for n in range(1, max(A)+1):
-                if n not in A:
-                    return n
-            return max(A)+1
+    a = set(A)
+    smallest_int = 1
+    while smallest_int in a:
+        smallest_int += 1
+    return smallest_int
+
+test_arr = list(range(1, 100000+1))
+random.shuffle(test_arr)
+print(solution(test_arr))
